5970816057f2af7750d6991a28ce921153a475b8
Dilpreet <n.dilpreetsingh@gmail.com>
2024-07-16 16:46:45 +0530

    removing app secrets file from github

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const supabaseUrl = 'https://vjhwpaxvmmcclmzyrgzz.supabase.co';
-  static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqaHdwYXh2bW1jY2xtenlyZ3p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExMjQ4NzQsImV4cCI6MjAzNjcwMDg3NH0.Yi9S5zju3M5hDqLdO_woeGLqhjBWRC3sxxFtP8gcPrM';
-}
